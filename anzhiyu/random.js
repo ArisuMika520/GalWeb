7d439f623788f190df519e566d617e02400f8bb4
arisumika <1907653220@qq.com>
2025-04-29 01:51:51 +0800

    Site updated: 2025-04-29 01:51:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/27/Docs/about_notice/","2025/04/28/getlink/getlink/"];function toRandomPost(){
+var posts=["2025/04/27/Docs/about_notice/","2025/04/29/Docs/download/","2025/04/29/Docs/feedbackproblem/","2025/04/29/Docs/fromproblem/","2025/04/28/getlink/getlink/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
