1b61b47db257d9aeeec0c3acbfda0a67c152fff4
arisumika <1907653220@qq.com>
2025-04-30 00:55:23 +0800

    Site updated: 2025-04-30 00:55:23

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/27/Docs/about_notice/","2025/04/29/Docs/download/","2025/04/29/Docs/feedbackproblem/","2025/04/29/Docs/fromproblem/","2025/04/29/Teach/解压/","2025/04/29/Tools/tool/","2025/04/28/getlink/getlink/","2025/04/29/Teach/mdf/"];function toRandomPost(){
+var posts=["2025/04/27/Docs/about_notice/","2025/04/29/Docs/download/","2025/04/29/Teach/mdf/","2025/04/29/Docs/feedbackproblem/","2025/04/29/Docs/fromproblem/","2025/04/29/Tools/tool/","2025/04/28/getlink/getlink/","2025/04/29/Teach/解压/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
