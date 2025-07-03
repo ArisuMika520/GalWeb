160ad770d68e51391feb7d017ddfff29a997e1fb
arisumika <1907653220@qq.com>
2025-07-03 16:20:32 +0800

    Site updated: 2025-07-03 16:20:29

---
+++ js/start.js
@@ -0,0 +1,55 @@
+Swal.fire
+(
+    {  
+    title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
+    html: "<a href='https://bs2806.trevovalkrespexil.com/bk/40914/n5sv61dll' target='_blank'><img src='https://r2.sakinori.top/0.png'><p>萝莉社 未满18禁止入内呦 漫画动漫真人集于一体的被窝神器！！！</p></a>",
+    footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
+    icon: "",  
+    allowOutsideClick: false,  
+    showConfirmButton: true,  
+    showCancelButton: false, 
+    confirmButtonText: "好的哦~",  
+    cancelButtonText: "补药！",
+    confirmButtonColor: "#f791a9",
+    theme: 'dark',
+    draggable: true
+}
+)
+/*.then((result) => 
+{
+    if (result.isConfirmed)
+    {
+        Swal.fire
+        ({
+            title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
+            html: "<a href='https://msb6x.com/game/mdetail?id=1879515764744374789&dc=mzdt0062' target='_blank'><img src='https://r2.sakinori.top/%E5%8C%85%E8%A3%85%E5%B0%81%E9%9D%A2%E6%8F%92%E7%94%BB.png'></a>",
+            footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
+            allowOutsideClick: false,  
+            showConfirmButton: true,  
+            showCancelButton: false, 
+            confirmButtonText: "看看下一个~",  
+            cancelButtonText: "补药！",
+            confirmButtonColor: "#f791a9",
+            theme: 'dark',
+            draggable: true
+        }).then((result) => 
+            {
+                if (result.isConfirmed)
+                {
+                    Swal.fire
+                    ({
+                        title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
+                        html: "<a href='https://msb6x.com/game/mdetail?id=1879515764744374789&dc=mzdt0062' target='_blank'><img src='https://r2.sakinori.top/%E8%BE%A3%E5%A6%B9%E5%92%8C%E9%9B%A8%E6%BB%B4%E9%AB%98%E7%94%BB%E8%B4%A8%E5%B0%81%E7%BB%98.jpg'></a>",
+                        footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
+                        allowOutsideClick: false,  
+                        showConfirmButton: true,  
+                        showCancelButton: false, 
+                        confirmButtonText: "看完了哦~",  
+                        confirmButtonColor: "#f791a9",
+                        theme: 'dark',
+                        draggable: true
+                    });
+                }
+            });
+    }
+})*/
