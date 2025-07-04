6fab7f907b1841cd964290cfb13668658a49f1c0
arisumika <1907653220@qq.com>
2025-07-05 02:10:08 +0800

    Site updated: 2025-07-05 02:10:04

--- js/start.js
+++ js/start.js
@@ -2,37 +2,37 @@ Swal.fire
 (
     {  
     title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-    html: "<a href='https://bs2806.trevovalkrespexil.com/bk/40914/n5sv61dll' target='_blank'><img src='https://r2.sakinori.top/0.png'><p>萝莉社 未满18禁止入内呦 漫画动漫真人集于一体的被窝神器！！！</p></a>",
+    html: "<a href='https://aigirlfriendstudio.com/?ref_id=88f10d5a-aa3a-47a1-b850-94927bf7ba2f' target='_blank'><img src='https://r2.sakinori.top/400x800.gif' width='300' height='600'></a>",
     footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
     icon: "",  
     allowOutsideClick: false,  
     showConfirmButton: true,  
     showCancelButton: false, 
-    confirmButtonText: "好的哦~",  
+    confirmButtonText: "看看下一个~",  
     cancelButtonText: "补药！",
     confirmButtonColor: "#f791a9",
     theme: 'dark',
     draggable: true
 }
 )
-/*.then((result) => 
+.then((result) => 
 {
     if (result.isConfirmed)
     {
         Swal.fire
         ({
             title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-            html: "<a href='https://msb6x.com/game/mdetail?id=1879515764744374789&dc=mzdt0062' target='_blank'><img src='https://r2.sakinori.top/%E5%8C%85%E8%A3%85%E5%B0%81%E9%9D%A2%E6%8F%92%E7%94%BB.png'></a>",
+            html: "<a href='https://msb6x.com/game/mdetail?id=1879515764744374789&dc=mzdt0062' target='_blank'><img src='https://r2.sakinori.top/0.png'><p>萝莉社 未满18禁止入内呦 漫画动漫真人集于一体的被窝神器！！！</p></a>",
             footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
             allowOutsideClick: false,  
             showConfirmButton: true,  
             showCancelButton: false, 
-            confirmButtonText: "看看下一个~",  
+            confirmButtonText: "好的哦~",  
             cancelButtonText: "补药！",
             confirmButtonColor: "#f791a9",
             theme: 'dark',
             draggable: true
-        }).then((result) => 
+        })/*.then((result) => 
             {
                 if (result.isConfirmed)
                 {
@@ -50,6 +50,6 @@ Swal.fire
                         draggable: true
                     });
                 }
-            });
+            });*/
     }
-})*/
+})
