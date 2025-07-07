b778adb57d3a24376c699cef2032e24b76259646
arisumika <1907653220@qq.com>
2025-07-07 20:44:11 +0800

    Site updated: 2025-07-07 20:44:08

--- js/start.js
+++ js/start.js
@@ -2,7 +2,7 @@ Swal.fire
 (
     {  
     title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-    html: "<a href='https://aigirlfriendstudio.com/?ref_id=88f10d5a-aa3a-47a1-b850-94927bf7ba2f' target='_blank'><img src='https://r2.sakinori.top/400x800.gif' width='250' height='500'></a>",
+    html: "<a href='https://bs2806.turlotremixdruzan.com/yj/40912/va1orubk' target='_blank'><img src='https://r2.sakinori.top/1920x1080%E7%9A%84%E5%89%AF%E6%9C%AC.png'><p>哔咔漫画 最新黄漫每日更新 打开二次元新世界</p></a>",
     footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
     icon: "",  
     allowOutsideClick: false,  
@@ -22,7 +22,7 @@ Swal.fire
         Swal.fire
         ({
             title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-            html: "<a href='https://msb6x.com/game/mdetail?id=1879515764744374789&dc=mzdt0062' target='_blank'><img src='https://r2.sakinori.top/0.png'><p>萝莉社 未满18禁止入内呦 漫画动漫真人集于一体的被窝神器！！！</p></a>",
+            html: "<a href='https://aigirlfriendstudio.com/?ref_id=88f10d5a-aa3a-47a1-b850-94927bf7ba2f' target='_blank'><img src='https://r2.sakinori.top/400x800.gif' width='250' height='500'></a>",
             footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
             allowOutsideClick: false,  
             showConfirmButton: true,  
