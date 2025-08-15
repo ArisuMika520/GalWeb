331651dd8ef3e1cd3e0df5d5a8ec9da022ad0eab
arisumika <1907653220@qq.com>
2025-08-16 06:38:06 +0800

    Site updated: 2025-08-16 06:38:01

--- js/start.js
+++ js/start.js
@@ -1,9 +1,9 @@
 Swal.fire
 (
     {  
-    title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-    html: "<a href='https://adh.quimbadravfryvryc8jlonex.com/yj/40912/va1orubk' target='_blank'><img src='https://r2.sakinori.top/1920x1080%E7%9A%84%E5%89%AF%E6%9C%AC.png'><p>哔咔漫画 最新黄漫每日更新 打开二次元新世界</p></a>",
-    footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
+    title: "<p style='color: #f791a9;'>重要公告！！！</p>",
+    html: "<img src='https://r2.sakinori.top/3.webp'>",
+    footer: "<p style='color: #f791a9;'>დ感谢支持！新网站&存储开发！დ</p>",
     icon: "",  
     allowOutsideClick: false,  
     showConfirmButton: true,  
@@ -22,7 +22,7 @@ Swal.fire
         Swal.fire
         ({
             title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-            html: "<a href='https://aigirlfriendstudio.com/?ref_id=88f10d5a-aa3a-47a1-b850-94927bf7ba2f' target='_blank'><img src='https://r2.sakinori.top/400x800.gif' width='250' height='500'></a>",
+            html: "<a href='https://adh.quimbadravfryvryc8jlonex.com/yj/40912/va1orubk' target='_blank'><img src='https://r2.sakinori.top/1920x1080%E7%9A%84%E5%89%AF%E6%9C%AC.png'><p>哔咔漫画 最新黄漫每日更新 打开二次元新世界</p></a>",
             footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
             allowOutsideClick: false,  
             showConfirmButton: true,  
@@ -32,14 +32,14 @@ Swal.fire
             confirmButtonColor: "#f791a9",
             theme: 'dark',
             draggable: true
-        })/*.then((result) => 
+        }).then((result) => 
             {
                 if (result.isConfirmed)
                 {
                     Swal.fire
                     ({
-                        title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",  
-                        html: "<a href='https://msb6x.com/game/mdetail?id=1879515764744374789&dc=mzdt0062' target='_blank'><img src='https://r2.sakinori.top/%E8%BE%A3%E5%A6%B9%E5%92%8C%E9%9B%A8%E6%BB%B4%E9%AB%98%E7%94%BB%E8%B4%A8%E5%B0%81%E7%BB%98.jpg'></a>",
+                        title: "<p style='color: #f791a9;'>❤广告点点谢谢喵！♡</p>",   
+                        html: "<a href='https://aigirlfriendstudio.com/?ref_id=88f10d5a-aa3a-47a1-b850-94927bf7ba2f' target='_blank'><img src='https://r2.sakinori.top/400x800.gif' width='250' height='500'></a>",
                         footer: "<p style='color: #f791a9;'>დ感谢支持！所有费用将用于网站开销和新网站&存储的开发！დ</p>",
                         allowOutsideClick: false,  
                         showConfirmButton: true,  
@@ -50,6 +50,6 @@ Swal.fire
                         draggable: true
                     });
                 }
-            });*/
+            });
     }
 })
